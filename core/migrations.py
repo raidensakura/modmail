f7a4ef12fbd0deaf3ac4a4858777f3d5df74881d
Khakers <22665282+khakers@users.noreply.github.com>
2023-07-17 16:08:27 -0700

    clean up migrations.py code

--- core/migrations.py
+++ core/migrations.py
@@ -1,26 +1,39 @@
 import datetime
 import re
+from typing import Optional
 
 from core import blocklist
 from core.models import getLogger
 
 logger = getLogger(__name__)
 
-old_format_matcher = re.compile("by ([\w]*#[\d]{1,4})(?: until <t:(\d*):f>)?.")
+old_format_matcher = re.compile("by (\w*#\d{1,4})(?: until <t:(\d*):f>)?.")
 
 
-def _convert_legacy_v2_block_format(k, v: dict, block_type: blocklist.BlockType) -> blocklist.BlocklistEntry:
+def _convert_legacy_dict_block_format(k, v: dict, block_type: blocklist.BlockType) -> Optional[blocklist.BlocklistEntry]:
+    """
+    Converts a legacy dict based blocklist entry to the new dataclass format
+
+    Returns None if the block has expired
+    """
+
     blocked_by = int(v["blocked_by"])
     if "until" in v:
         # todo make sure this is the correct from format
         blocked_until = datetime.datetime.fromisoformat(v["until"])
+        # skip if blocked_until occurred in the past
+        if blocked_until < datetime.datetime.now(datetime.timezone.utc):
+            return None
     else:
         blocked_until = None
+
     if "reason" in v:
         reason = v["reason"]
     else:
         reason = None
+
     blocked_ts = datetime.datetime.fromisoformat(v["blocked_at"])
+
     return blocklist.BlocklistEntry(
         id=int(k),
         expires_at=blocked_until,
@@ -31,12 +44,20 @@ def _convert_legacy_v2_block_format(k, v: dict, block_type: blocklist.BlockType)
     )
 
 
-def _convert_legacy_block_format(k, v: str, block_type: blocklist.BlockType) -> blocklist.BlocklistEntry:
+def _convert_legacy_block_format(k, v: str, block_type: blocklist.BlockType) -> Optional[blocklist.BlocklistEntry]:
+    """
+    Converts a legacy string based blocklist entry to the new dataclass format
+
+    Returns None if the block has expired
+    """
+
     match = old_format_matcher.match(v)
-    # blocked_by = match.group(1)
     blocked_until = match.group(2)
     if blocked_until is not None:
         blocked_until = datetime.datetime.fromtimestamp(int(blocked_until), tz=datetime.timezone.utc)
+        # skip if blocked_until occurred in the past
+        if blocked_until < datetime.datetime.now(datetime.timezone.utc):
+            return None
 
     return blocklist.BlocklistEntry(
         id=int(k),
@@ -56,39 +77,22 @@ async def _convert_legacy_block_list(foo: dict, blocklist_batch: list[blocklist.
     for k, v in foo.items():
         # handle new block format
         if type(v) is dict:
-            block = _convert_legacy_v2_block_format(k, v, block_type=block_type)
-            if block.expires_at is not None and block.expires_at < datetime.datetime.now(datetime.timezone.utc):
+            block = _convert_legacy_dict_block_format(k, v, block_type=block_type)
+            if block is None:
                 logger.debug("skipping expired block entry")
                 skipped += 1
                 continue
-            logger.debug(f"migrating new format {v}")
-            blocklist_batch.append(block)
+            logger.debug(f"migrating new format {k}: {v}")
+        else:
+            block = _convert_legacy_block_format(k, v, block_type=block_type)
+            if block is None:
+                logger.debug("skipping expired block entry")
+                skipped += 1
+                continue
+            logger.debug(f"migrating legacy format {k}: {v}")
 
-            continue
+        blocklist_batch.append(block)
 
-        match = old_format_matcher.match(v)
-        blocked_by = match.group(1)
-        blocked_until = match.group(2)
-        if blocked_until is not None:
-            blocked_until = datetime.datetime.fromtimestamp(int(blocked_until), tz=datetime.timezone.utc)
-
-        # skip this item if blocked_until occurred in the past
-        if blocked_until is not None and blocked_until < datetime.datetime.now(datetime.timezone.utc):
-            logger.debug("skipping expired block entry")
-            skipped += 1
-            continue
-
-        logger.debug(f"migrating id:{k} ts:{blocked_until} blocker:{blocked_by}")
-
-        blocklist_batch.append(blocklist.BlocklistEntry(
-            id=int(k),
-            expires_at=blocked_until,
-            reason=f"migrated from old format `{v}`",
-            timestamp=datetime.datetime.utcnow(),
-            # I'm not bothering to fetch the user object here, discords username migrations will have broken all of them
-            blocking_user_id=0,
-            type=blocklist.BlockType.USER
-        ))
         if len(blocklist_batch) >= 100:
             await bot.api.db.blocklist.insert_many([x.__dict__ for x in blocklist_batch])
             blocklist_batch.clear()
@@ -100,21 +104,26 @@ async def migrate_blocklist(bot):
     start_time = datetime.datetime.utcnow()
 
     blocked_users = bot.blocked_users
-    logger.info(f"preparing to migrate blocklist")
+    logger.info("preparing to migrate blocklist")
     skipped = 0
 
     blocklist_batch: list[blocklist.BlocklistEntry] = []
     logger.info(f"preparing to process {len(blocked_users)} blocked users")
     skipped += await _convert_legacy_block_list(foo=blocked_users, blocklist_batch=blocklist_batch,
                                                 block_type=blocklist.BlockType.USER, bot=bot)
-    logger.info(f"processed blocked users")
+    logger.info("processed blocked users")
     logger.info(f"preparing to process {len(bot.blocked_roles)} blocked roles")
     skipped += await _convert_legacy_block_list(foo=bot.blocked_roles, blocklist_batch=blocklist_batch,
                                                 block_type=blocklist.BlockType.ROLE, bot=bot)
-    logger.info(f"processed blocked roles")
+    logger.info("processed blocked roles")
 
     await bot.api.db.blocklist.insert_many([x.__dict__ for x in blocklist_batch])
     blocklist_batch.clear()
+
+    logger.info("clearing old blocklists")
+    bot.blocked_users.clear()
+    bot.blocked_roles.clear()
+    await bot.config.update()
 
     logger.info(f"Migration complete! skipped {skipped} entries")
     logger.info(f"migrated in {datetime.datetime.utcnow() - start_time}")
