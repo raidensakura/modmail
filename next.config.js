4243285bc535bf2c5ce08f4f1d74aaf2788c7bd3
Raiden <raiden@project-mei.xyz>
2025-12-07 00:59:05 +0800

    Refactor next.config.js to use require syntax

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-// next.config.js
-module.exports = (async () => {
-  const nextraModule = await import('nextra')
-  const nextra = nextraModule.default || nextraModule
-  const withNextra = nextra({
-    theme: 'nextra-theme-docs',
-    themeConfig: './theme.config.jsx',
-  })
-  // withNextra is a function that returns the Next config
-  return withNextra()
-})()
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+})
+
+module.exports = withNextra()
