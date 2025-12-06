8497f4a89da9ee2a9a20d45840be8952a5d48364
Raiden <raiden@project-mei.xyz>
2025-12-07 00:46:10 +0800

    Refactor next.config.js for dynamic nextra import
    Refactor next.config.js to use dynamic import for nextra.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
-
-module.exports = withNextra()
+// next.config.js
+module.exports = (async () => {
+  const nextraModule = await import('nextra')
+  const nextra = nextraModule.default || nextraModule
+  const withNextra = nextra({
+    theme: 'nextra-theme-docs',
+    themeConfig: './theme.config.jsx',
+  })
+  // withNextra is a function that returns the Next config
+  return withNextra()
+})()
